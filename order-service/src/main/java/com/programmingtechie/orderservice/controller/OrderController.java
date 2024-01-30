2ba0f7d99a5f6a1616778e4357990e2b35a7194e
jagadeeshroyal <rjroyal2@gmail.com>
2024-01-30 21:25:10 +0530

    After circuit Breaker implemetation

--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.programmingtechie.orderservice.controller;
 
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,10 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory",fallbackMethod = "placeOrderFailureMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         return orderService.placeOrder(orderRequest);
     }
+    public String placeOrderFailureMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return "Unable to process your request at the moment";}
 }
