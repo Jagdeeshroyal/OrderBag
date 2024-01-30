d8e4d4f45d1945549d4d787026d4b059e702c5ca
jagadeeshroyal <rjroyal2@gmail.com>
2024-01-30 21:31:11 +0530

    Branch initialized and will start kafka now

--- order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/programmingtechie/orderservice/controller/OrderController.java
@@ -3,9 +3,13 @@ package com.programmingtechie.orderservice.controller;
 import com.programmingtechie.orderservice.dto.OrderRequest;
 import com.programmingtechie.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -16,10 +20,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "inventory",fallbackMethod = "placeOrderFailureMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        return orderService.placeOrder(orderRequest);
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(()-> orderService.placeOrder(orderRequest));
     }
-    public String placeOrderFailureMethod(OrderRequest orderRequest, RuntimeException runtimeException){
-        return "Unable to process your request at the moment";}
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please order after some time!");
+    }
 }
